ebfdcd07010aa422b5e68cf92da4444637f8e4c3
Karanjsk2k01 <karanvarun14501@gmail.com>
2023-10-27 09:46:14 +0530

    taken form data to local storage

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ loginForm.addEventListener("submit", (e) => {
 
   console.log(`${name.value}, ${email.value}, ${Password.value}, ${timeForCall.value}, ${time.value}`);
 
+  //local storage setting item
+  localStorage.setItem('name', `${name.value}`);
+  localStorage.setItem('email', `${email.value}`);
+  localStorage.setItem('Password', `${Password.value}`);
+  localStorage.setItem('timeForCall', `${timeForCall.value}`);
+  localStorage.setItem('time', `${time.value}`);
+
+
   name.value = ''
   email.value = ''
   Password.value = ''
@@ -27,4 +35,6 @@ loginForm.addEventListener("submit", (e) => {
 // li.style.background = 'LightGreen';
 // ul.lastElementChild.innerHTML = '<h2>Hello</h2>'
 // ul.children[1].style.background = 'yellow'
+//practising cookies
 
+// document.cookie = 'name:karan;expires=' + new Date(2024, 0, 1).toUTCString();
